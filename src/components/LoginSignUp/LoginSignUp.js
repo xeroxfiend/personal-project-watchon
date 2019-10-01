3f5c2b291e3daaf850b2e76cfdbc7b464ef70231
Brian Lowe <mynameisboy@gmail.com>
2019-10-01 16:35:49 -0600

    auth front end working

--- src/components/LoginSignUp/LoginSignUp.js
+++ src/components/LoginSignUp/LoginSignUp.js
@@ -1,23 +1,99 @@
-import React, {Component} from 'react'
+import React, {Component} from "react";
+import axios from "axios";
+import swal from "sweetalert2";
+import store, {ADD_USER} from "../../store";
 
 class LoginSignUp extends Component {
-    constructor() {
-        super()
-        this.state = {
-            email: '',
-            password: ''
-        }
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleChange(value, key) {
+    this.setState({
+      [key]: value
+    });
+  }
+
+  register() {
+    if (!this.state.email || !this.state.password)
+      return swal.fire("Email and Password cannot be blank");
+
+    axios
+      .post("/auth/register", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(res => {
+        store.dispatch({
+          type: ADD_USER,
+          payload: {
+            userId: res.data.user.userId[0].user_id,
+            userEmail: this.state.email
+          }
+        });
+        this.props.history.goBack();
+      })
+      .catch(err => {
+        if (err.response.status === 404)
+          return swal.fire("User already exists");
+      });
+  }
+
+  async login() {
+    if (!this.state.email || !this.state.password)
+      return swal.fire("Email and Password cannot be blank");
+    let res;
+    try {
+      res = await axios.post("/auth/login", {
+        email: this.state.email,
+        password: this.state.password
+      });
+    } catch (err) {
+      if (err.response.status === 404 || err.response.status === 403) {
+        return swal.fire("Incorrect Email or Password");
+      } else {
+        return swal.fire("Unknown error!");
+      }
     }
 
+    store.dispatch({
+      type: ADD_USER,
+      payload: {
+        userId: res.data.user.userId,
+        userEmail: this.state.email
+      }
+    });
 
-    render() {
-        return (
-            <div className="login-sign-up">
-                LoginSignUp!
-            </div>
-        )
-    }
+    if (!res.data.user) return swal.fire(res.data.message);
+    this.props.history.goBack();
+  }
+
+  render() {
+    return (
+      <div className="login-sign-up">
+        <input
+          onChange={e => this.handleChange(e.target.value, "email")}
+          type="text"
+          className="auth-email"
+        />
+        <input
+          onChange={e => this.handleChange(e.target.value, "password")}
+          type="text"
+          className="auth-password"
+        />
+        <button onClick={() => this.login()} className="login">
+          Login
+        </button>
+        <button onClick={() => this.register()} className="register">
+          Register
+        </button>
+      </div>
+    );
+  }
 }
 
-
-export default LoginSignUp
+export default LoginSignUp;
