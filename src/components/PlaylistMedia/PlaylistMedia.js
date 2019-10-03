7dc865d48c1f6411f9fdd93b653b935b0a96ac73
Brian Lowe <mynameisboy@gmail.com>
2019-10-03 10:49:50 -0600

    got rating stars working on front end

--- src/components/PlaylistMedia/PlaylistMedia.js
+++ src/components/PlaylistMedia/PlaylistMedia.js
@@ -2,8 +2,8 @@ import React, {Component} from "react";
 import axios from "axios";
 import store from "../../store";
 import swal from "sweetalert2";
-import starIcon from '../../assets/star.png'
-import './playlistmedia.css'
+import starIcon from "../../assets/star.png";
+import "./playlistmedia.css";
 
 class PlaylistMedia extends Component {
   constructor() {
@@ -11,7 +11,7 @@ class PlaylistMedia extends Component {
     const reduxState = store.getState();
     this.state = {
       userId: reduxState.userId,
-      rating: null
+      rating: 0
     };
   }
 
@@ -27,10 +27,15 @@ class PlaylistMedia extends Component {
   }
 
   handleRating(num) {
-      this.setState({
-          rating: num
-      })
-      console.log(this.state)
+    if (this.state.rating === 1 && num === 1) {
+      return this.setState({
+        rating: 0
+      });
+    }
+
+    this.setState({
+      rating: num
+    });
   }
 
   render() {
@@ -59,11 +64,36 @@ class PlaylistMedia extends Component {
         <h3>Rating: {this.props.data.rating}</h3>
         <h3 className="available-playlist">Available on: {mappedLocations}</h3>
         <div className="star-rating">
-            <img onClick={() => this.handleRating(1)} src={starIcon} alt="star" className="star 1"/>
-            <img onClick={() => this.handleRating(2)} src={starIcon} alt="star" className="star 2"/>
-            <img onClick={() => this.handleRating(3)} src={starIcon} alt="star" className="star 3"/>
-            <img onClick={() => this.handleRating(4)} src={starIcon} alt="star" className="star 4"/>
-            <img onClick={() => this.handleRating(5)} src={starIcon} alt="star" className="star 5"/>
+          <img
+            onClick={() => this.handleRating(1)}
+            src={starIcon}
+            alt="star"
+            className={this.state.rating >= 1 ? "filled-star" : "empty-star"}
+          />
+          <img
+            onClick={() => this.handleRating(2)}
+            src={starIcon}
+            alt="star"
+            className={this.state.rating >= 2 ? "filled-star" : "empty-star"}
+          />
+          <img
+            onClick={() => this.handleRating(3)}
+            src={starIcon}
+            alt="star"
+            className={this.state.rating >= 3 ? "filled-star" : "empty-star"}
+          />
+          <img
+            onClick={() => this.handleRating(4)}
+            src={starIcon}
+            alt="star"
+            className={this.state.rating >= 4 ? "filled-star" : "empty-star"}
+          />
+          <img
+            onClick={() => this.handleRating(5)}
+            src={starIcon}
+            alt="star"
+            className={this.state.rating >= 5 ? "filled-star" : "empty-star"}
+          />
         </div>
         <button onClick={() => this.remove()} className="remove">
           Remove
